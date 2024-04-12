19857ceffff95508e08d44735a916b243fe87090
Baiel1208 <baiel000005@gmail.com>
2024-04-12 19:06:23 +0600

    перевод модели и дополнение

--- apps/job/models.py
+++ apps/job/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    title = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    earnings = models.DecimalField(max_digits=10, decimal_places=2)
-    start_date = models.DateField()
-    description = models.TextField()
-    image = models.ImageField(upload_to='job_images/')
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
+    location = models.CharField(max_length=100, null=False, blank=False, verbose_name='Местоположение')
+    earnings = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, verbose_name='Заработок')
+    start_date = models.DateField(null=False, blank=False, verbose_name='Начало дня работы')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='job_images/', null=True, blank=True, verbose_name='Картинка')
+
+
+    def __str__(self):
+        return self.title 
+    
+    class Meta:
+        verbose_name = "Актуальная работа"
+        verbose_name_plural = "Актуальные работы"
